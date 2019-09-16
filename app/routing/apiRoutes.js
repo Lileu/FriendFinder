ff595ed1ce479c5fe86285d34cb80195375c7c13
Lisa Leu <Lisal@netwealth.com.au>
2019-09-16 12:37:34 +1000

    added custom style.css to survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,47 +4,48 @@ var friendsData = require("../data/friends");
 // ROUTING
 // ===============================================================================
 
-module.exports = function (app) {
-    // retrieve all friends data in json format
-    app.get("/api/friends", function (req, res) {
-        res.json(friendsData);
-    });
+module.exports = function(app) {
+  // retrieve all friends data in json format
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
 
+  // handle incoming survey results
+  app.post("/api/friends", function(req, res) {
+    // req.body property parses incoming request bodies in a middleware before handlers
+    // record all user input
+    var userData = req.body;
+    var userScores = userData.scores;
+    var totalDifference = 0;
 
-    // handle incoming survey results
-    app.post('/api/friends', function (req, res) {
-        // req.body property parses incoming request bodies in a middleware before handlers
-        // record all user input
-        var userData = req.body;
-        var userScores = userData.scores;
-        var totalDifference = 0;
+    // compute match
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
 
-        // compute match
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        }
+    // for each friend in the db
+    for (var i = 0; i < friendsData.length; i++) {
+      totalDifference = 0;
+      // calculate the difference between their score and the user's score
 
-        // for each friend in the db
-        for (var i = 0; i < friendsData.length; i++) {
-            totalDifference = 0;
-            // calculate the difference between their score and the user's score
-
-            for (var j = 0; j < userScores.length; j++) {
-                totalDifference += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(userScores[j]));
-            }
-            // find the friend with the lowest score variance and declare that friend as the match
-            if (totalDifference <= bestMatch.friendDifference) {
-
-                bestMatch.friendDifference = totalDifference;
-                bestMatch.name = friendsData[i].name;
-                bestMatch.photo = friendsData[i].photo;
-            }
-        }
-        // add new user to db
-        friendsData.push(userData);
-        // send response with the match data
-        res.json(bestMatch);
-    });
+      for (var j = 0; j < userScores.length; j++) {
+        totalDifference += Math.abs(
+          parseInt(friendsData[i].scores[j]) - parseInt(userScores[j])
+        );
+      }
+      // find the friend with the lowest score variance and declare that friend as the match
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.friendDifference = totalDifference;
+        bestMatch.name = friendsData[i].name;
+        bestMatch.photo = friendsData[i].photo;
+      }
+    }
+    // add new user to db
+    friendsData.push(userData);
+    //console.log(userData);
+    // send response with the match data
+    res.json(bestMatch);
+  });
 };
